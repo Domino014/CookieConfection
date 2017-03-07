3d4bfcbbe7b313e3ae39f27eb2eef3a78abe0054
Megan Evans <medomino4@gmail.com>
2017-03-06 21:35:28 -0700

    second attempt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
-app.listen(process.env PORT || 3000, function(){  //3000 port is reserved for development
+app.listen(process.env.PORT || 3000, function(){  //3000 port is reserved for development
 	console.log("listening on 80 or 3000");
 });
 
