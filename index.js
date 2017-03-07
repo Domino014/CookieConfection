5f1735c33036edbc4bbf8e49967d9b5758b3be2f
Megan Evans <medomino4@gmail.com>
2017-03-06 21:20:24 -0700

    index.js

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ app.listen(process.env PORT || 3000, function(){  //3000 port is reserved for de
 npm init  to create package json
 npm install --save express
 
+go to folder Cookie Confectionary
+heroku login
+enter email
+enter password
+heroku create
+
+Megans-MacBook-Air:Cookie Confectionary meganevans$ heroku create
+Creating app... done, ⬢ thawing-headland-96719
+git init
+git status
+git add .
+git status
+*/
+
+
